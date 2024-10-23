a2c4028b0373f24fc0f20da3b2ffafb7046dde1c
Duize08 <00001091762557sp@al.educacao.sp.gov.br>
2024-10-23 10:26:05 -0300

    Add files via upload
    pj fn

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filme',
+    'Qual o nome do dragão da mãe do suloço?',
+    'Pula nuvem'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o menor país do mundo',
+    'Vaticano'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a área de um circulo cuja o diâmetro é 10',
+    '78,539'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz chocolate em Inglês?',
+    'Chocolate em ingles é chocolate'
+)
